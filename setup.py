f72fd25b8840f10f3c24bcf2ba136d5a4501b730
rene.f.aguirre <rene.f.aguirre@55bbceea-385e-11de-b438-597f59cd7555>
2009-09-28 15:29:58 +0000

    Just starting new version, some patches to come
    git-svn-id: http://pywinusb.googlecode.com/svn/trunk@17 55bbceea-385e-11de-b438-597f59cd7555

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 from setuptools import setup, find_packages
-VERSION = '0.1.0'
+VERSION = '0.2.0'
 README = os.path.join(os.path.dirname(__file__), 'README.txt')
 long_description = open(README).read() + 'nn'
 setup(name='pywinusb',
